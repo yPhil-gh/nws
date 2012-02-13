754736ff0975e496f0dc256ae569cf8723c3f18c
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-02-13 16:31:56 +0000

    NWS

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+/*
+Index : View feeds
+Time-stamp: <index.php - Mon 13-Feb-2012 16:30:23>
+
+This script is part of NWS
+
+NWS is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 <?
 if (isset($_GET['code'])) { die(highlight_file(__FILE__, 1)); }
 ?>
--- nws-reload-feed.php
+++ nws-reload-feed.php
@@ -1,3 +1,24 @@
+/*
+reload-feed : Reload one feed
+Time-stamp: <nws-reload-feed.php - Mon 13-Feb-2012 16:31:06>
+
+This script is part of NWS
+
+NWS is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 <?php
 
 function debug ($var) {
