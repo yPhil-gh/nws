099301529d66b4378dd3b19a12c28935bca2505b
xaccrocheur <xaccrocheur@gmail.com>
2013-09-06 17:09:10 +0100

    JQuery 1.9 online

--- index.php
+++ index.php
@@ -40,10 +40,8 @@ if (isset($_GET['code'])) { die(highlight_file(__FILE__, 1)); }
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 
-
-
-<script src="nws/jquery.min.js"></script>
-<script src="nws/jquery-ui.min.js"></script>
+<!--script src="nws/jquery.min.js"></script>
+<script src="nws/jquery-ui.min.js"></script-->
 
 
     <script>
