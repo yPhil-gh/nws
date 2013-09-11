3103b87f230f5fe7610862bc765ffd96b0e92018
xaccrocheur <xaccrocheur@gmail.com>
2013-09-11 19:12:23 +0100

    Now binary safe

--- nws-favicon.php
+++ nws-favicon.php
@@ -6,17 +6,6 @@
   https://github.com/xaccrocheur/nws/
 
 */
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>nws</title>
-</head>
-<body>
-
-<?php
 
 function CheckImageExists($imgUrl) {
     if (@GetImageSize($imgUrl)) {
