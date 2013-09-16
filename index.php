57b75c0f8aefa5ce87c1270265cace18a2347594
xaccrocheur <xaccrocheur@gmail.com>
2013-09-16 15:51:14 +0100

    NWS 0.5 is GO

--- index.php
+++ index.php
@@ -119,8 +119,8 @@ echo '
 $commits = json_decode(file_get_contents("https://api.github.com/repos/xaccrocheur/nws/commits"));
 
 $current_commit_minus1 = $commits[1]->sha;
-$ref_commit = "31c554473b68d345745e4a6518ffa96cd4c8ce13";
-$commit_message = "last message : ".$commits[0]->commit->message.;
+$ref_commit = "f49517108fda5f7ea5d6f34e71eb218d0c1d6798";
+$commit_message = "last message : ".$commits[0]->commit->message;
 
 if (!strcmp($current_commit_minus1, $ref_commit)) {
     $version_class = "unmoved";
