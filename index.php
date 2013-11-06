390e83e089af0280f07b4679ffe0b4b9711a8645
xaccrocheur <xaccrocheur@gmail.com>
2013-11-06 12:46:35 +0000

    (much) Tighter layout

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ echo '
 $commits = json_decode(file_get_contents("https://api.github.com/repos/xaccrocheur/nws/commits"));
 
 $current_commit_minus1 = $commits[1]->sha;
-$ref_commit = "f68d1774a4974248ac5dd60b01b245d7b166abb1";
+$ref_commit = "771eeb3f3c9673efb802fd92fd5ae818bf36a4f7";
 $commit_message = "last message : ".$commits[0]->commit->message;
 
 if (!strcmp($current_commit_minus1, $ref_commit)) {
