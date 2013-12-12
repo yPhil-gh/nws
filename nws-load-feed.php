13ad3c7cfcf23fdb6874bb8e1a55412d15dd70e1
xaccrocheur <xaccrocheur@gmail.com>
2013-12-12 15:33:30 +0100

    New Atom image SRC attribute (for dumb yahoo pipes feeds)

--- nws-load-feed.php
+++ nws-load-feed.php
@@ -228,9 +228,9 @@ function reparse($u, $numItems, $imgMode, $photoblog, $max_age) {
                 $title = strip_tags($item->title);
                 $imgSrc = str_img_src($item->description);
 
-                // Image
+                // Images
+                $atomImg = (isset($item->enclosure['url']) ? $item->enclosure['url'] : $item->enclosure['src']);
 
-                $atomImg = $item->enclosure['url'];
                 $elseSrc = str_img_src(strip_tags($item->content, "<img>"));
 
                 //Use that namespace
@@ -259,16 +259,19 @@ function reparse($u, $numItems, $imgMode, $photoblog, $max_age) {
                         $elseSrc = 'http://'.$domain.$elseSrc;
                     }
                 }
+
                 if (empty($imgSrc) && !empty($elseSrc))
                     $imgSrc = $elseSrc;
 
                 if ($imgMode == 'none' || ($imgMode == 'first' && $i > 1)) {
                     $img = '';
                 }
-                elseif ($photoblog) {
+                if ($photoblog) {
                     $img = '<a href="'.$imgSrc.'"><img class="full" alt="'.$title.'" src="'.$imgSrc.'" /></a>';
                     $title = $title;
-                } elseif (!empty($atomImg)) {
+                }
+
+                if (!empty($atomImg)) {
                     $ext = pathinfo($atomImg, PATHINFO_EXTENSION);
                     if ($ext == "mp3")
                         $img = '<a href="'.$atomImg.'"><span class="audio-note" title="Audio content">♫</span></a>';
