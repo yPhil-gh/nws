08dc923e640124bba7e6fe98ccd958e12cc7e52a
xaccrocheur <xaccrocheur@gmail.com>
2014-06-27 17:54:21 +0100

    xHTML Validation

--- index.php
+++ index.php
@@ -456,7 +456,7 @@ if (empty($urls)) {
         echo '
     <div id="viewer">
       <a id="link-img">
-        <img id="viewer-img" alt="Gallery viewer" />
+        <img id="viewer-img" alt="Gallery viewer" src="" />
       </a>
       <span id="cross" title="Close" aria-hidden="true" class="icon-close"></span>
       <span id="img-name"><a></a></span>
@@ -486,7 +486,7 @@ $current_commits = file_get_contents("https://api.github.com/repos/xaccrocheur/n
 if ($current_commits !== false) {
     $commits = json_decode($current_commits);
 
-    $ref_commit = "046a1845c43b1a296f7735ff1642f8d08b394f29";
+    $ref_commit = "13c551f0440c15c3031ffa18f520ea31ccc0863e";
 
     $current_commit_minus1 = $commits[1]->sha;
     $commit_message = "last message : ".$commits[0]->commit->message;
